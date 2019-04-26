d2ba2511ed329ecf884ea726502d5a269d257129
ASzot <gstenger98@gmail.com>
2019-04-26 03:14:10 -0700

    readme update

--- webapp/app/models.py
+++ webapp/app/models.py
@@ -3,12 +3,6 @@ from app import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-
-# stu_course_identifier = db.Table('stu_co_identifier',
-#     db.Column('course_id', db.Integer, db.ForeignKey('courses.id')),
-#     db.Column('user_id', db.Integer, db.ForeignKey('users.id'))
-# )
-
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
